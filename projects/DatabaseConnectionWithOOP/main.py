fb4aaa348b7d913fca2f47c5d43ba7ba591d68f3
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-22 15:54:37 -0600

    Database and Cursor classes integration for database connection and queries execution (Unstable): 0.2

--- projects/DatabaseConnectionWithOOP/main.py
+++ projects/DatabaseConnectionWithOOP/main.py
@@ -1,33 +1,4 @@
-from . classes import Cursor, Database
-from . classes import INTERFACES
-
-# Custom cursor for most common queries execution.
-class CustomCursor(Cursor, INTERFACES.I_CustomSelectQuery):
-    def __init__(self, database_object:Database):
-        super().__init__(database_object)
-
-    def execute_query(self, query:str):
-        """
-            Executes a simple query of any type wether SELECT, INSERT, CREATE TABLE, DELETE, etc.
-            This method leverages its class's parent class method with the same name, behavior and attributes.
-
-            Attributes:
-                query (str): The query that will be executed.
-        """
-        super().execute_query(query)
-    
-    def execute_simple_select_query(self, table_name:str, fields:list[str]) -> list:
-        table_name = table_name
-        fields = fields
-        query = f"""SELECT {[field for field in fields]} FROM {table_name};"""
-        data = self.execute_query(query)
-        return data
-    
-    def create_table(self, table_name:str, fields:list[str]) -> None:
-        self.execute_query("")
-        pass 
-        """Uncompleted yet"""
-
+from classes import Database, CustomCursor
 
 
 """ Now with a simple cursor object we can execute the most common queries like the typical 'SELECT', or create a table with ease reducing the SQL statement we have to provide to just parameters. """
@@ -35,8 +6,20 @@ my_database = Database('MyDatabase')
 my_cursor = CustomCursor(my_database)
 
 # Creating users table
-my_cursor.create_table('users')
+my_cursor.create_table('users', ['id INTEGER PRIMARY KEY AUTOINCREMENT', 'username TEXT UNIQUE NOT NULL', 'email TEXT UNIQUE NOT NULL', 'password TEXT NOT NULL', 'age INTEGER'])
+
+# Inserting data in users table
+users_to_insert = [
+    ['Joaquin', 'joaquin@gmail.com', '123password', 22], 
+    ['Aline', 'aline@gmail.com', '123password', 21], 
+    ['Henry', 'henry@gmail.com', '123password', 35], 
+    ['James', 'james@gmail.com', '123password', 32], 
+    ['Nicole', 'nicole@gmail.com', '123password', 28],
+    ['Maria', 'maria@gmail.com', '123password', 38] 
+]
+users_fields = ['username', 'email', 'password', 'age']
+my_cursor.insert_rows('users', users_fields, users_to_insert)
 
 # Fetching data from database in users table
-result = my_cursor.execute_simple_select_query('users', ['username', 'email', 'password', 'age'])
+result = my_cursor.execute_simple_select_query('users', users_fields)
 print(result)
